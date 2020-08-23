d1f50eb1f3a3e5e25bfefcd315ccf3faa8cfd9d4
Hugo Holm <hugo.tiger.holm@gmail.com>
2020-08-23 16:34:11 +0200

    FIX: Reduced margin-bottom

--- client/src/components/counter/styles.js
+++ client/src/components/counter/styles.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 const Counter = styled.div`
   width: 350px;
   height: 68px;
-  margin-bottom: 32px;
+  margin-bottom: 16px;
 
   /* This matches h1-styling, but i'm using <p> as it's
      more semantically correct
