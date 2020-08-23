fb9b339c2e5fdd9e421e71d8793f45e9ddd0c3c7
Hugo Holm <hugo.tiger.holm@gmail.com>
2020-08-23 16:40:04 +0200

    Removed refetchingof total

--- client/src/hooks/useAllTimeTotal.js
+++ client/src/hooks/useAllTimeTotal.js
@@ -1,18 +1,15 @@
-import { useState, useContext, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { getAllBeers } from "../services/beersApi";
 import { sumBeersAmount } from "../utils";
-import { BeerContext } from "../context/beers/BeerContext";
 
 export const useAllTimeTotal = () => {
-  const { beers } = useContext(BeerContext);
   const [total, setTotal] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(null);
   const [error, setError] = useState(null);
 
-  const length = (beers && Math.floor(beers.length / 10)) || 0;
   useEffect(() => {
-    console.log("körs");
     const fetchBeers = () => {
+      setIsLoading(true);
       getAllBeers()
         .then((res) => {
           if (res.error) setError(res.data);
@@ -23,7 +20,7 @@ export const useAllTimeTotal = () => {
     };
 
     fetchBeers();
-  }, [length]);
+  }, []);
 
   return { total, isLoading, error };
 };
