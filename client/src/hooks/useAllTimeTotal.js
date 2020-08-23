c40976f07f710309970fa5b00b03744038a07c06
Hugo Holm <hugo.tiger.holm@gmail.com>
2020-08-23 16:35:00 +0200

    Added naive solution for updating total-counter

--- client/src/hooks/useAllTimeTotal.js
+++ client/src/hooks/useAllTimeTotal.js
@@ -1,15 +1,18 @@
-import { useState, useEffect } from "react";
+import { useState, useContext, useEffect } from "react";
 import { getAllBeers } from "../services/beersApi";
 import { sumBeersAmount } from "../utils";
+import { BeerContext } from "../context/beers/BeerContext";
 
 export const useAllTimeTotal = () => {
+  const { beers } = useContext(BeerContext);
   const [total, setTotal] = useState(null);
-  const [isLoading, setIsLoading] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
 
+  const length = (beers && Math.floor(beers.length / 10)) || 0;
   useEffect(() => {
+    console.log("körs");
     const fetchBeers = () => {
-      setIsLoading(true);
       getAllBeers()
         .then((res) => {
           if (res.error) setError(res.data);
@@ -20,7 +23,7 @@ export const useAllTimeTotal = () => {
     };
 
     fetchBeers();
-  }, []);
+  }, [length]);
 
   return { total, isLoading, error };
 };
